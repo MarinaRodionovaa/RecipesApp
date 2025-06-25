c4ca953673b09ccff9719cf38ec239544c433fe6
marina_rodionova <marina_rodionova@>
2025-06-25 20:32:04 +0300

    RA-04 fix and create first fragment with categories

---
+++ app/src/main/java/ru/marinarodionova/recipesapp/CategoriesListFragment.kt
@@ -0,0 +1,29 @@
+package ru.marinarodionova.recipesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.marinarodionova.recipesapp.databinding.FragmentCategoriesListBinding
+
+class CategoriesListFragment : Fragment() {
+
+    private var _binding: FragmentCategoriesListBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCategoriesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/ru/marinarodionova/recipesapp/MainActivity.kt
+++ app/src/main/java/ru/marinarodionova/recipesapp/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.marinarodionova.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = R.layout.activity_main
         super.onCreate(savedInstanceState)
-        setContentView(binding)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.mainFragmentContainer, CategoriesListFragment())
+                .commit()
         }
     }
 }
